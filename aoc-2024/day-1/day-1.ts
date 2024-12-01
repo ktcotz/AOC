8040e2dcbb2765febde3b79f27aae6c35c5e8ddf
ktcotz <naskret.kamil@gmail.com>
2024-12-01 17:25:02 +0100

    feat: day-1 aoc
    Work with day-1 aoc, part one and two.

--- aoc-2024/day-1/day-1.ts
+++ aoc-2024/day-1/day-1.ts
@@ -2,4 +2,52 @@ import { loadInput } from "../../utils/utils";
 
 const input = loadInput("input");
 
-console.log(input);
+const partOne = () => {
+  const firstListNumbers: number[] = [];
+  const secondListNumbers: number[] = [];
+  let result = 0;
+
+  input.split("\n").forEach((line) => {
+    const [x, y] = line.split(" ".repeat(3));
+
+    if (x && y) {
+      firstListNumbers.push(Number(x));
+      secondListNumbers.push(Number(y));
+    }
+  });
+
+  const sortedOne = firstListNumbers.sort((a, b) => (a -= b));
+  const sortedTwo = secondListNumbers.sort((a, b) => (a -= b));
+
+  sortedOne.forEach((item, idx) => {
+    result += Math.abs(item - sortedTwo[idx]);
+  });
+
+  console.log(result);
+};
+
+const partTwo = () => {
+  const firstListNumbers: number[] = [];
+  const secondListNumbers: number[] = [];
+
+  input.split("\n").forEach((line) => {
+    const [x, y] = line.split(" ".repeat(3));
+
+    if (x && y) {
+      firstListNumbers.push(Number(x));
+      secondListNumbers.push(Number(y));
+    }
+  });
+
+  const result = firstListNumbers
+    .map((number) => {
+      const itemsInSecond = secondListNumbers.filter((item) => item === number);
+
+      return number * itemsInSecond.length;
+    })
+    .reduce((acc, next) => (acc += next), 0);
+
+  console.log(result);
+};
+
+partTwo();
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,4 +1,4 @@
 import { readFileSync } from "fs";
 
-export const loadInput = (file: string = "input"): string =>
+export const loadInput = (file: string = "input") =>
   readFileSync(file, { encoding: "utf-8" });
